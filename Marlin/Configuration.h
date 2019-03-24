36dd15c19f249c142a626a293e4670474e12f1bf
Michael Muscato <>
2019-03-23 17:15:14 -0700

    Updating acceleration and jerk for better quality.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -543,7 +543,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 200, 1000 }
+#define DEFAULT_MAX_ACCELERATION      { 500, 500, 200, 1000 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
@@ -565,10 +565,10 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define DEFAULT_XJERK                 20.0
-#define DEFAULT_YJERK                 20.0
-#define DEFAULT_ZJERK                  0.4
-#define DEFAULT_EJERK                  5.0
+#define DEFAULT_XJERK                 5.0
+#define DEFAULT_YJERK                 5.0
+#define DEFAULT_ZJERK                 0.4
+#define DEFAULT_EJERK                 5.0
 
 //===========================================================================
 //============================= Z Probe Options =============================
